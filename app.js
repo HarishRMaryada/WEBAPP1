adb2fab41c44e9313d65fa897638478c4571a841
Harish R M <harishmaryada@outlook.com>
2018-09-21 21:06:33 +0530

    Parsers
    added Parsers

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+//custom middleware
 const logger = require('./MiddleWare/logger-middleware')
 
 var app = express()
@@ -6,16 +9,20 @@ var app = express()
 app.set('port',process.env.PORT||7979)
 
 
+app.use(cookieParser())
+
+//parse application /www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+//parse application/json
+app.use(bodyParser.json())
 
 logger(app)
-// app.use(function(req, res, next) {
-//     console.log(req.ip);
-//
-//     next();
-// })
-app.get('/data',(req,res)=>{
-  console.log(req.params)
+
+
+app.post('/',(req,res)=>{
+  //console.log(req.params)
   console.log(req.body)
+  res.json(req.body)
 })
 
 //404 Error
@@ -28,10 +35,8 @@ app.use(function(req,res) {
 
 // custom 500 page
 app.use(function(err, req, res, next){
-console.error(err.stack)
-res.type('text/plain')
-res.status(500)
-res.send('500 - Server Error')
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
 })
 
 
